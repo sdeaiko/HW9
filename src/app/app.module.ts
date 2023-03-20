f0c7dee690d61215478c2bce66c6b41949291a96
deaiko <s.deaiko@gmail.com>
2023-03-20 19:23:23 +0600

    v. 0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { WhyComponent } from './why/why.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { CtaComponent } from './cta/cta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    WhyComponent,
+    AchievementComponent,
+    TestimonialComponent,
+    CtaComponent
   ],
   imports: [
     BrowserModule
